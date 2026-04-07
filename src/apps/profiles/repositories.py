37c1b2720b8083db156ef4cb8aace80818e2ddac
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2026-04-07 18:19:59 -0300

    Create ProfileRepository

---
+++ src/apps/profiles/repositories.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+from .models import NutritionalProfile
+
+
+class ProfileRepository:
+    @staticmethod
+    def update_targets(
+        profile: NutritionalProfile, bmr: Decimal, daily_target: Decimal
+    ) -> NutritionalProfile:
+        profile.bmr = bmr  # type: ignore
+        profile.daily_calorie_target = daily_target  # type: ignore
+
+        profile.save(update_fields=["bmr", "daily_calorie_target", "updated_at"])
+
+        return profile
