89c34fd64d5e4269a23d173bb6c126d9a7e266fc
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2026-04-07 21:33:23 -0300

    Basic testing for ProfileServiceTests

--- src/apps/profiles/tests.py
+++ src/apps/profiles/tests.py
@@ -1,3 +1,17 @@
+from decimal import Decimal
 from django.test import TestCase
+from apps.profiles.services import ProfileService
 
-# Create your tests here.
+class ProfileServiceTests(TestCase):
+    def setUp(self):
+        self.service = ProfileService(repository=None)
+
+    def test_calculate_bmr_for_male(self):
+        # Peso: 80kg, Altura: 180cm, Idade: 30 anos
+        result = self.service.calculate_bmr(Decimal("80"), 180, 30, "M")
+
+        self.assertEqual(result, Decimal("1780.00"))
+
+    def test_calculate_bmr_raises_error_for_invalid_sex(self):
+        with self.assertRaises(ValueError):
+            self.service.calculate_bmr(Decimal("80"), 180, 30, "X")
