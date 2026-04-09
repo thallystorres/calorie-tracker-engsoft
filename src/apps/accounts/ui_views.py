39f7889d3f9f7b224a2c1e68a269fade3eeb9862
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2026-04-08 23:13:29 -0300

    fix: Remove unrelated template from accounts app

--- src/apps/accounts/ui_views.py
+++ src/apps/accounts/ui_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
-from apps.foods.models import Food, MealLog
+
 
 @require_GET
 def register_page(request):
@@ -42,19 +42,3 @@ def password_reset_confirm_page(request, token: str):
 @require_GET
 def password_reset_success_page(request):
     return render(request, "accounts/password_reset_success.html")
-
-@require_GET
-@login_required
-def nutritional_profile_page(request):
-    return render(request, "accounts/nutritional_profile.html")
-
-@require_GET
-@login_required
-def tracker_page(request):
-
-    foods = Food.objects.all().order_by("name")
-    meal_logs = MealLog.objects.filter(user=request.user).order_by("-consumed_at")
-    return render(request, "accounts/tracker.html", {
-      "foods": foods,
-      "meal_logs": meal_logs
-    })
