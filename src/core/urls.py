534153897a65d4bcaa069cabeb72312deeba4d74
lucasaamorim <lucas.apolonio.125@ufrn.edu.br>
2026-04-08 23:15:48 -0300

    refactor: update UI related routes

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,6 +4,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.ui_urls")),
+    path("foods/", include("apps.foods.ui_urls")),
+    path("profiles/", include("apps.profiles.ui_urls")),
     path("api/accounts/", include("apps.accounts.urls")),
     path("api/profiles/", include("apps.profiles.urls")),
     path("api/foods/", include("apps.foods.urls")),
